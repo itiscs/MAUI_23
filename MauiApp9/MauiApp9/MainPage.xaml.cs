816fa8bc801473350abcbad539eb00830984df9d
itiscs <moudroff@gmail.com>
2023-10-24 16:41:29 +0300

    Init

---
+++ MauiApp9/MauiApp9/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using System.Drawing;
+
+namespace MauiApp9
+{
+    public partial class MainPage : ContentPage
+    {
+     
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            Ellipse el = new Ellipse();
+            el.WidthRequest = 150;
+            el.HeightRequest = 150;
+            el.Fill = Microsoft.Maui.Graphics.Color.
+                FromRgb(r.Next(255), r.Next(255), r.Next(255));
+
+            stk.Children.Add(el);
+            
+        }
+    }
+}
---
+++ MauiApp9/MauiApp9/SecondPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp9;
+
+public partial class SecondPage : ContentPage
+{
+	public SecondPage()
+	{
+		InitializeComponent();
+	}
+}
